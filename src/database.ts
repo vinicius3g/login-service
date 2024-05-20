2d767762959c8240171e2770441394eaa15173ed
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2024-05-20 16:15:00 -0300

    feat(database): conexão com o banco de dados utilizando sequelize

---
+++ src/database.ts
@@ -0,0 +1,18 @@
+import { Sequelize } from 'sequelize';
+import 'dotenv/config'
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+export const sequelize = new Sequelize({
+  dialect: 'postgres',
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  ssl: true,
+  dialectOptions: {
+    ssl: {
+      readonly: true,
+    }
+  }
+});
