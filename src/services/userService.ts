d0f398a5aba64e3e75150ff4389831c3c98c4637
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2024-05-20 21:34:05 -0300

    feat(services): implementaçao de - bcrypt para reforço de senhas - jwt para validaçao de login - serviço de login

---
+++ src/services/userService.ts
@@ -0,0 +1,42 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { User } from '../models/userModel';
+import { generateToken } from '../utils/auth';
+
+const SALT_ROUNDS = 10;
+
+interface CreateUserParams {
+    username: string;
+    email: string;
+    password: string;
+}
+
+interface LoginUserParams {
+    email: string;
+    password: string;
+}
+
+export const createUser = async ({ username, email, password }: CreateUserParams) => {
+    const password_hash = await bcrypt.hash(password, SALT_ROUNDS);
+
+    const newUser = await User.create({
+        username,
+        email,
+        password_hash
+    });
+
+    return newUser;
+};
+
+export const loginUser = async ({ email, password }: LoginUserParams) => {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user || !await bcrypt.compare(password, user.password_hash)) {
+        throw new Error('Credenciais inválidas');
+    }
+    
+
+    const token = generateToken({ id: user.id, email: user.email });
+
+    return token;
+};
