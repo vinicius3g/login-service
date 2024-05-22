6564137e83da985f4e92fa2a4ad5f4f20b18c085
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2024-05-22 10:22:50 -0300

    feat(routes): rota de verificaçao de usuario logado

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import { createUser, loginUser } from '../controllers/userController';
+import { createUser, getProfile, loginUser } from '../controllers/userController';
+import { authenticateJWT } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
 router.post('/', createUser);
 router.post('/login', loginUser);
+router.get('/profile', authenticateJWT, getProfile);
 
 export default router;
