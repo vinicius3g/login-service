e1796a57da5d468cccb9807591130dcd9628de74
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2024-05-22 10:23:37 -0300

    feat(controller): serviço responsavel por retornar as informaçoes do usuario logado

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import * as userService from '../services/userService';
+import { AuthRequest } from '../middleware/authMiddleware';
 
 export const createUser = async (req: Request, res: Response) => {
     const { username, email, password } = req.body;
@@ -22,5 +23,13 @@ export const loginUser = async (req: Request, res: Response) => {
     } catch (error) {
         console.error('Erro ao autenticar usuário:', error);
         res.status(401).json({ error: 'Credenciais inválidas' });
+    }
+};
+
+export const getProfile = async (req: AuthRequest, res: Response) => {
+    if (req.user) {
+        res.status(200).json(req.user);
+    } else {
+        res.status(404).json({ error: 'Usuário não encontrado' });
     }
 };
